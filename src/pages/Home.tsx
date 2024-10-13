de6437cf3cc0e0aeefbc0bcddd16c83fad44642e
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-10-13 23:50:59 +0200

    cleaned the files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { UserContext } from '../context/UserContext';
 import LoginForm from '../components/LoginForm';
 import { Link } from 'react-router-dom';
