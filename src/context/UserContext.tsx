79c5fc6bc2c8c4be37abf31f3495d732b1d8cf1f
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-10-14 21:18:58 +0200

    Rename temp.tsx back to UserContext.tsx

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode, useEffect } from 'react';
+import React, { createContext, useState, ReactNode, useEffect } from 'react';
 
 export interface Movie {
   imdbID: string;
