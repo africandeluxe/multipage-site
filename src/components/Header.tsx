ce3a11b6812802826f158aa0c023ab072bf1aa20
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-10-25 23:37:18 +0200

    Restricting navigation when logged out

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,28 @@
 import { Link } from 'react-router-dom';
-
-export {};
-
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
 const Header = () => {
+  const context = useContext(UserContext);
+
+  if (!context) {
+    return null;
+  }
+
+  const { loggedIn, userName } = context;
+
   return (
     <header className="bg-oliveGreen text-lightCream p-4 flex justify-between items-center">
       <h1 className="text-2xl font-bold">Movie Watchlist</h1>
-      <nav>
-        <ul className="flex space-x-4">
-          <li><Link to="/profile" className="hover:text-warmTan">Home</Link></li>
-          <li><Link to="/profile" className="hover:text-warmTan">Profile</Link></li>
-          <li><Link to="/categories" className="hover:text-warmTan">Categories</Link></li>
-        </ul>
-      </nav>
+      {loggedIn ? (
+        <nav>
+          <ul className="flex space-x-4">
+            <li><Link to="/home" className="hover:text-warmTan">Home</Link></li>
+            <li><Link to="/profile" className="hover:text-warmTan">Profile</Link></li>
+            <li><Link to="/categories" className="hover:text-warmTan">Categories</Link></li>
+          </ul>
+        </nav>
+      ) : null}
     </header>
   );
 };
