adacbebe133ea4403fecf39724cc4e4dfb510f23
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-10-25 23:11:42 +0200

    Fixed navigation with <Link> components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom';
+
 export {};
+
 
 const Header = () => {
   return (
@@ -6,9 +9,9 @@ const Header = () => {
       <h1 className="text-2xl font-bold">Movie Watchlist</h1>
       <nav>
         <ul className="flex space-x-4">
-          <li><a href="/" className="hover:text-warmTan">Home</a></li>
-          <li><a href="/profile" className="hover:text-warmTan">Profile</a></li>
-          <li><a href="/categories" className="hover:text-warmTan">Categories</a></li>
+          <li><Link to="/profile" className="hover:text-warmTan">Home</Link></li>
+          <li><Link to="/profile" className="hover:text-warmTan">Profile</Link></li>
+          <li><Link to="/categories" className="hover:text-warmTan">Categories</Link></li>
         </ul>
       </nav>
     </header>
